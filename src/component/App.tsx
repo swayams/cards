5e40fd6d791d1628317b5ae9362e4cf22a42001e
swayams1 <32326113+swayams1@users.noreply.github.com>
2019-01-07 13:57:46 +0530

    initial commit

---
+++ src/component/App.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+export interface ICardProps {
+    
+}
+ 
+export interface ICardState {
+    
+}
+ 
+class Card extends React.Component<ICardProps, ICardState> {
+    constructor(props: ICardProps) {
+        super(props);
+        this.state = { :  };
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Card;
